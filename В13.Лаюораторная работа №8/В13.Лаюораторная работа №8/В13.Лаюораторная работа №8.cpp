1fd82957faa29329f62e2714f09170c0af29e7b0
AlisaTarasova <76028528+AlisaTarasova@users.noreply.github.com>
2021-05-22 14:42:43 +0500

    Add files via upload

---
+++ В13.Лаюораторная работа №8/В13.Лаюораторная работа №8/В13.Лаюораторная работа №8.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+void func(int N, int k)
+{
+    int k1;
+    if (k==1)
+    {
+        k1 = 0;
+         
+}
+
+
+int main()
+{
+    system("color F0");
+    setlocale(0, "");
+    int n, k, N;
+    cout << "Введите количество разрядов в вдоичном числе: ";
+    cin >> N;
+    k = 1;
+    func(N,k);
+
+}
+
